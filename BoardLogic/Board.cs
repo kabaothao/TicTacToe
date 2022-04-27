c85ec04627b0f18d7a61df804a74d80faba20f77
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-27 15:43:53 -0500

    updated codes

---
+++ BoardLogic/Board.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardLogic
+{
+    public class Board
+    {
+        public int[] Grid
+        { get; set; }
+
+        public Board()
+        {
+            Grid = new int[9];
+
+            //initalize the Grid
+            // initalize the array and set all squares to empty
+            for (int i = 0; i < 9; i++)
+            {
+                Grid[i] = 0;
+            }
+
+        }
+
+        public bool isBoardFull()
+        {
+            bool isFull = true;
+            //assume the board is full. if an exception is found, change the value to false.
+            for (int i = 0; i < Grid.Length; i++)
+            {
+                if (Grid[i] == 0)
+                {
+                    isFull = false;
+
+                }
+            }
+            return isFull;
+        }
+        public int checkForWinner()
+        {
+            //return a 0 if nobody won. return the player number if they won.
+
+            //top row
+            if (Grid[0] == Grid[1] && Grid[1] == Grid[2])
+            {
+                return Grid[0];
+            }
+
+            //second row
+            if (Grid[0] == Grid[4] && Grid[4] == Grid[5])
+            {
+                return Grid[3];
+            }
+
+            //third row
+            if (Grid[6] == Grid[7] && Grid[7] == Grid[8])
+            {
+                return Grid[6];
+            }
+
+            //first column
+            if (Grid[0] == Grid[3] && Grid[3] == Grid[6])
+            {
+                return Grid[0];
+            }
+
+            //second column
+            if (Grid[1] == Grid[4] && Grid[4] == Grid[7])
+            {
+                return Grid[1];
+            }
+
+            //third column
+            if (Grid[2] == Grid[5] && Grid[5] == Grid[8])
+            {
+                return Grid[2];
+            }
+
+            //first diagonal
+            if (Grid[0] == Grid[4] && Grid[4] == Grid[8])
+            {
+                return Grid[0];
+            }
+
+            //second diagonal
+            if (Grid[2] == Grid[4] && Grid[4] == Grid[6])
+            {
+                return Grid[2];
+            }
+
+
+            return 0;
+        }
+    }
+
+
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,112 +1,72 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using BoardLogic;
 using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace TicTacToe // Note: actual namespace depends on the project name.
 {
     public class Program
     {
-        
+
         // board is the data store for the game
-        static int[] board = new int[9]; // show 9 tiles
+        static Board game = new Board();
+
+
         static void Main(string[] args)
         {
-            // set all squares to empty
-            for (int i = 0; i < 9; i++)
-            {
-                board[i] = 0;
-            }
 
+            //set up some undefines values
             int userTurn= -1;
             int computerTurn= -1;
+
             Random rand = new Random(); //the computer will choose something for you. 
 
-
-            while (checkForWinner() == 0) 
+            // keep playing until someone wins
+            while (game.checkForWinner() == 0) 
             {
-                //don't allow the human to choose an already occupied square
-                while (userTurn == -1 || board[userTurn] != 0)
+                // don't allow the human to choose an already occupied square
+                // get valid input from the user 
+                while (userTurn == -1 || game.Grid[userTurn] != 0)
                 {
                     Console.WriteLine("Please enter a number from 0 to 8");
                     userTurn = int.Parse(Console.ReadLine());
                     Console.WriteLine("You typed " + userTurn);
                 }
 
-                board[userTurn] = 1;
+                game.Grid[userTurn] = 1;
+
+                if (game.isBoardFull())
+                    break;
+
+
 
                 //don't let the computer pick an invalid number
-                while(computerTurn == -1 || board[computerTurn] != 0)
+                // get a random move from the computer
+                while(computerTurn == -1 || game.Grid[computerTurn] != 0)
                 {
                     computerTurn = rand.Next(8); //pick a random number 
                     Console.WriteLine("Computer chooses " + computerTurn);
                 }
+                game.Grid[computerTurn] = 2; //assign to the board
 
-                board[computerTurn] = 2; //assign to the board
-
+                // show the board
                 printBoard();
 
             }
 
-            Console.WriteLine("Player " + checkForWinner() +  " won the game!");
+            // while is done
+            Console.WriteLine("The game is over. Player " + game.checkForWinner() +  " is the winner");
+            Console.ReadLine();
+
+            //Console.WriteLine("Player " + checkForWinner() +  " won the game!");
+
+           
+
 
 
         }
 
-        private static int checkForWinner()
-        {
-            //return a 0 if nobody won. return the player number if they won.
-            
-            //top row
-            if (board[0] == board[1] && board[1] == board[2])
-            {
-                return board[0];
-            }
 
-            //second row
-            if (board[0] == board[4] && board[4] == board[5])
-            {
-                return board[3];
-            }
-
-            //third row
-            if (board[6] == board[7] && board[7] == board[8])
-            {
-                return board[6];
-            }
-
-            //first column
-            if (board[0] == board[3] && board[3] == board[6])
-            {
-                return board[0];
-            }
-
-            //second column
-            if (board[1] == board[4] && board[4] == board[7])
-            {
-                return board[1];
-            }
-
-            //third column
-            if (board[2] == board[5] && board[5] == board[8])
-            {
-                return board[2];
-            }
-
-            //first diagonal
-            if (board[0] == board[4] && board[4] == board[8])
-            {
-                return board[0];
-            }
-
-            //second diagonal
-            if (board[2] == board[4] && board[4] == board[6])
-            {
-                return board[2];
-            }
-
-
-            return 0;
-        }
 
         private static void printBoard()
         {
@@ -118,15 +78,15 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 // print x or o for each sqaure
                 // 0 means unoccupied. 1 means player 1 (X) 2 means player 2 (O)
 
-                if (board[i] == 0)
+                if (game.Grid[i] == 0)
                 {
                     Console.Write(".");
                 }
-                if (board[i] == 1)
+                if (game.Grid[i] == 1)
                 {
                     Console.Write("X");
                 }
-                if (board[i] == 2)
+                if (game.Grid[i] == 2)
                 {
                     Console.Write("O");
                 }
@@ -138,6 +98,8 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 }
 
             }
+
+            Console.WriteLine("...----------------------------------");
         }
 
     }
